0a0f92b7d6ee240a569c82dd62b5aabc722eab8f
Lukáš Koudelík <l_koudelik@utb.cz>
2025-05-23 00:24:37 +0200

    Update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.footballservice.app',
   appName: 'Fotbalový servis',
   webDir: 'www',
   plugins: {
