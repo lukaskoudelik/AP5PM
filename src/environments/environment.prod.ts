9f0dd2421012b2ee04c7557d29d16ac201ebf6d3
Lukáš Koudelík <l_koudelik@utb.cz>
2025-05-23 01:08:01 +0200

    Remove environments folder from repo

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://nqfgwrumswyrtxuaasgo.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xZmd3cnVtc3d5cnR4dWFhc2dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYwOTM2MTUsImV4cCI6MjA1MTY2OTYxNX0.6VyZQ0fx-5P3hPpUQ5E3AJ6Uamol7p1iIf3Uu3UcYKU',
-};
