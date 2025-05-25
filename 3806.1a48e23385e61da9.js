7747bc1e4e4923082727788b1553ec4782b2cba0
Lukáš Koudelík <l_koudelik@utb.cz>
2025-05-25 05:03:07 +0200

    Deploy

---
+++ 3806.1a48e23385e61da9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[3806],{3806:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
